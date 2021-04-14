21ede3de1234218ea82ac15cb6a75da47f632227
Jesus Fuentes <jesusfuentes2808@gmail.com>
2021-04-14 17:36:32 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
+import { UsersModule } from './@admin/pages/users/users.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: ``, redirectTo: `home`, pathMatch: `full` },
+  { path: `**`, redirectTo: `home`, pathMatch: `full` }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+      routes,
+      {
+        useHash: true,
+        scrollPositionRestoration: 'enabled'
+      }
+    )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
