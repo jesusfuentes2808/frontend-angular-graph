2243e0a8dbb4e20656ad666f7b3e9434a13c4b42
Jesus Fuentes <jesusfuentes2808@gmail.com>
2021-04-19 17:21:13 -0500

    03 - Plantilla base de tienda

--- src/app/@admin/pages/admin.module.ts
+++ src/app/@admin/pages/admin.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-import { HeaderComponent } from '../../@admin/core/componentes/header/header.component';
-import { TitleComponent } from './../../@admin/core/componentes/title/title.component';
-import { SidebarComponent } from '../../@admin/core/componentes/sidebar/sidebar.component';
-
+import { HeaderComponent } from '@admin-core/componentes/header/header.component';
+import { TitleComponent } from '@admin-core/componentes/title/title.component';
+import { SidebarComponent } from '@admin-core/componentes/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: ``, redirectTo: `home`, pathMatch: `full` },
-  { path: `**`, redirectTo: `home`, pathMatch: `full` }
+  { 
+    path: ``, 
+    redirectTo: ``, 
+    pathMatch: `full` 
+  },
+  { 
+    path: `**`, 
+    redirectTo: `home`, 
+    pathMatch: `full` 
+  }
 ];
 
 @NgModule({
